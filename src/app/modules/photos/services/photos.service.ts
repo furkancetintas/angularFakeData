3840fefea43fd6004f7ad896dc3f48c43da31e0f
Furkan Çetintaş <furkancetintas01@gmail.com>
2022-06-25 22:59:58 +0300

    photos service added

--- src/app/modules/photos/services/photos.service.ts
+++ src/app/modules/photos/services/photos.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { CoreHttpService } from 'src/shared/services/http-core.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PhotosService {
+export class PhotosService extends CoreHttpService{
 
-  constructor() { }
+
+  override API_URL = `${environment.apiUrl}photos`;
+  constructor(@Inject(HttpClient) http:HttpClient) { 
+    super(http);
+  }
+
+  getPhotosMethod(){
+    const newPath = `${this.API_URL}`;
+    const request = this.getMethod(newPath);
+    return request;
+  }
 }
