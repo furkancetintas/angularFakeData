621286dfb17226fbdeebed0e75955a441716ac77
Furkan Çetintaş <furkancetintas01@gmail.com>
2022-06-25 22:10:58 +0300

    todos service added

--- src/app/modules/todos/services/todos.service.ts
+++ src/app/modules/todos/services/todos.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { TodoModel } from './../models/todo.model';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { CoreHttpService } from 'src/shared/services/http-core.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TodosService {
+export class TodosService extends CoreHttpService {
 
-  constructor() { }
+  override API_URL = `${environment.apiUrl}todos`;
+  constructor(@Inject(HttpClient) http: HttpClient) {
+    super(http);
+  }
+
+  getTodosMethod() : Observable<TodoModel[]> {
+    const newPath = `${this.API_URL}`;
+    const request = this.getMethod(newPath);
+    return request;
+  }
 }
