aba514ec980beea1587331a80b57425c4a6274b4
Furkan Çetintaş <furkancetintas01@gmail.com>
2022-06-25 22:11:22 +0300

    posts service added

--- src/app/modules/posts/services/posts.service.ts
+++ src/app/modules/posts/services/posts.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { CoreHttpService } from 'src/shared/services/http-core.service';
+import { Observable } from 'rxjs';
+import { PostModel } from '../models/post.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PostsService {
+export class PostsService extends CoreHttpService {
 
-  constructor() { }
+  override API_URL = `${environment.apiUrl}posts`;
+  constructor(
+    @Inject(HttpClient) http:HttpClient) { 
+      super(http);
+    }
+  
+
+  getPostsMethod() : Observable<PostModel[]> {
+    const newPath = `${this.API_URL}`;
+    const request = this.getMethod(newPath);
+    return request;
+
+  }
 }
