17501027b89dc472109dab2fdc6e54d5c1ed8f4b
Furkan Çetintaş <furkancetintas01@gmail.com>
2022-06-26 01:43:53 +0300

    comments service added

--- src/app/modules/comments/services/comments.service.ts
+++ src/app/modules/comments/services/comments.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { CoreHttpService } from 'src/shared/services/http-core.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CommentsService {
+export class CommentsService extends CoreHttpService{
 
-  constructor() { }
+  override API_URL = `${environment.apiUrl}comments`;
+
+  constructor(@Inject(HttpClient) http:HttpClient) { 
+    super(http);
+  }
+
+  getCommentsMethod(){
+    const newPath = `${this.API_URL}`;
+    const request = this.getMethod(newPath);
+    return request;
+  }
 }
