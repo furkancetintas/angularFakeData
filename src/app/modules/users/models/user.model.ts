9cb97372510b2b4a514a188081646f445eae6c22
Furkan Çetintaş <furkancetintas01@gmail.com>
2022-06-26 17:26:01 +0300

    user model added

--- src/app/modules/users/models/user.model.ts
+++ src/app/modules/users/models/user.model.ts
@@ -1,3 +1,13 @@
+import { AddressModel } from "./address.model";
+import { CompanyModel } from "./company.model";
+
 export interface UserModel {
-    
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: AddressModel;
+    phone: string;
+    website: string;
+    company: CompanyModel;
 }
