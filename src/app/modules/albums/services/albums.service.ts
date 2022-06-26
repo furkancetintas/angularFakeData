d4e4bc0ff12a131f512ef09b9148329257e5810f
Furkan Çetintaş <furkancetintas01@gmail.com>
2022-06-26 13:51:11 +0300

    albums service added

--- src/app/modules/albums/services/albums.service.ts
+++ src/app/modules/albums/services/albums.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { CoreHttpService } from 'src/shared/services/http-core.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AlbumsService {
+export class AlbumsService extends CoreHttpService{
 
-  constructor() { }
+  override API_URL = `${environment.apiUrl}albums`;
+
+  constructor(@Inject(HttpClient) http:HttpClient) { 
+    super(http);
+  }
+
+  getAlbumsMethod(){
+    const newPath = `${this.API_URL}`;
+    const request = this.getMethod(newPath);
+    return request;
+  }
 }
