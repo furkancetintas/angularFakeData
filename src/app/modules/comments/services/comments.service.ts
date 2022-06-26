e6eb0b063f767abcceabf547940baf355bf65d08
Furkan Çetintaş <furkancetintas01@gmail.com>
2022-06-26 14:21:58 +0300

    comments service changed!

--- src/app/modules/comments/services/comments.service.ts
+++ src/app/modules/comments/services/comments.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { CoreHttpService } from 'src/shared/services/http-core.service';
+import { CommentModel } from '../models/comment.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +16,7 @@ export class CommentsService extends CoreHttpService{
     super(http);
   }
 
-  getCommentsMethod(){
+  getCommentsMethod() : Observable<CommentModel[]>{
     const newPath = `${this.API_URL}`;
     const request = this.getMethod(newPath);
     return request;
